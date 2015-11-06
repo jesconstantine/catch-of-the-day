daa11366ceb8dc1c91bd24ab089349795beef8f9
Jes Constantine <jesconstantine@gmail.com>
2015-11-06 13:57:49 -0500

    add react routes for app, storepicker, 404

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,18 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
+var ReactRouter = require('react-router');
+var Router  = ReactRouter.Router;
+var Route = ReactRouter.Route;
+var Navigation = ReactRouter.Navigation;
+var createBrowserHistory = require('history/lib/createBrowserHistory');
+
 
 /*
 	App
 */
 
-var App = React.createClass ({
+var App = React.createClass ({	
 	render : function () {
 		return (
 			<div className="catch-of-the-day">
@@ -30,10 +36,10 @@ var Header = React.createClass({
 		return (
 			<header className="top">
 				<h1>Catch 
-				<span className="ofThe">
-					<span className="of">of</span>
-					<span className="the">the</span> 
-				</span>
+					<span className="ofThe">
+						<span className="of">of</span>
+						<span className="the">the</span> 
+					</span>
 				Day</h1>
 				<h3 className="tagline"><span>{this.props.tagline}</span></h3>
 			</header>
@@ -76,13 +82,38 @@ var StorePicker = React.createClass({
 	render : function() {
 		return (
 			<form className="store-selector">
-			<h2>Please Enter A Store</h2>
-			<input type="text" ref="storeId" required />
-			<input type="Submit" />
+				<h2>Please Enter A Store</h2>
+				<input type="text" ref="storeId" required />
+				<input type="Submit" />
 			</form>
 		)
 	}
 
 });
 
-ReactDOM.render(<App/>, document.querySelector('#main'));
+/*
+	Not Found
+*/
+
+var NotFound = React.createClass({
+	render : function() {
+		return (
+			<h1>NotFound!</h1>
+		)
+	}
+})
+
+
+/*
+	Routes
+*/
+
+var routes = (
+  <Router history={createBrowserHistory()}>
+    <Route path="/" component={StorePicker}/>
+    <Route path="/store/:storeId" component={App}/>
+	<Route path="*" component={NotFound}/>
+  </Router>
+)
+
+ReactDOM.render(routes, document.querySelector('#main'));
