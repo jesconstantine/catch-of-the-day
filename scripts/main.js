8fe3ae178157859e036541dd16366f40caf6d239
Jes Constantine <jesconstantine@gmail.com>
2015-11-06 14:22:53 -0500

    add submit event listener to StorePicker form and route to storeId

--- scripts/main.js
+++ scripts/main.js
@@ -5,6 +5,8 @@ var ReactRouter = require('react-router');
 var Router  = ReactRouter.Router;
 var Route = ReactRouter.Route;
 var Navigation = ReactRouter.Navigation;
+
+var History = ReactRouter.History;
 var createBrowserHistory = require('history/lib/createBrowserHistory');
 
 var helpers = require('./helpers');
@@ -80,9 +82,21 @@ var Inventory = React.createClass({
 */
 
 var StorePicker = React.createClass({
+	mixins : [History],
+	goToStore : function(event) {
+		event.preventDefault(); // prevent form from submitting data
+		// get the data from the input
+		var storeId = this.refs.storeId.value; 
+		// this = storepicker
+		// refs = input reference
+		this.history.pushState(null, '/store/' + storeId);
+		// pushState allows browser to change URL without refresh, #, !, etc.
+		
+		// transition from <StorePicker/> to <App/>
+	},
 	render : function() {
 		return (
-			<form className="store-selector">
+			<form className="store-selector" onSubmit={this.goToStore}>
 				<h2>Please Enter A Store</h2>
 				<input type="text" ref="storeId" defaultValue={helpers.getFunName()} required />
 				<input type="Submit" />
