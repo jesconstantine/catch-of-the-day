47ad06776fea3fa98670f38651c4fd9265866f0d
Jes Constantine <jesconstantine@gmail.com>
2015-11-06 10:00:34 -0500

    add layout components

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,64 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
+
+/*
+	App
+*/
+
+var App = React.createClass ({
+	render : function () {
+		return (
+			<div className="catch-of-the-day">
+				<div className="menu">
+					<Header />
+				</div>
+				<Order />
+				<Inventory />
+			</div>
+		)
+	}
+});
+
+/* 
+	Header
+	<Header />
+*/
+
+var Header = React.createClass({
+	render : function() {
+		return (
+			<p>Header</p>
+		)
+	}
+});
+
+/* 
+	Order
+	<Order />
+*/
+
+var Order = React.createClass({
+	render : function() {
+		return (
+			<p>Order</p>
+		)
+	}
+});
+
+/* 
+	Inventory
+	<Inventory />
+*/
+
+var Inventory = React.createClass({
+	render : function() {
+		return (
+			<p>Inventory</p>
+		)
+	}
+});
+
 /*
 	StorePicker
 	this will let us make <StorePicker/>
@@ -9,10 +67,14 @@ var ReactDOM = require('react-dom');
 var StorePicker = React.createClass({
 	render : function() {
 		return (
-			<p>hi</p>
+			<form className="store-selector">
+			<h2>Please Enter A Store</h2>
+			<input type="text" ref="storeId" required />
+			<input type="Submit" />
+			</form>
 		)
 	}
 
 });
 
-ReactDOM.render(<StorePicker/>, document.querySelector('#main'));
+ReactDOM.render(<App/>, document.querySelector('#main'));
