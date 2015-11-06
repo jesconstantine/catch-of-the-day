6aed39f0e3f45cb0dc42ac7063a388f380a45967
Jes Constantine <jesconstantine@gmail.com>
2015-11-06 15:37:26 -0500

    display state, add fish component to app with sample fish data

--- scripts/main.js
+++ scripts/main.js
@@ -37,11 +37,23 @@ var App = React.createClass ({
 			fishes : require('./sample-fishes')
 		});
 	},
+	renderFish : function(key) {
+		return (
+			<Fish key={key} index={key} details={this.state.fishes[key]} />
+		)
+	},
 	render : function () {
 		return (
 			<div className="catch-of-the-day">
 				<div className="menu">
 					<Header tagline="Fresh Seafood Market" />
+					<ul className="list-of-fishes">
+						{
+							//Object.keys returns array of all keys in object
+							// run renderFish once for every fish that we have in state
+							Object.keys(this.state.fishes).map(this.renderFish)
+						}
+					</ul>
 				</div>
 				<Order />
 				<Inventory addFish={this.addFish} loadSamples={this.loadSamples} />
@@ -50,6 +62,27 @@ var App = React.createClass ({
 	}
 });
 
+
+/*
+	Fish
+	<Fish />
+*/
+
+var Fish = React.createClass({
+	render : function() {
+		var details = this.props.details;
+		return (
+			<li className="menu-fish">
+				<img src={details.image} alt={details.name} />
+				<h3 className="fish-name">
+					{details.name}
+					<span className = "price">{helpers.formatPrice(details.price)}</span>
+				</h3>
+				<p>{details.desc}</p>
+			</li>
+		)
+	}
+});
 
 /* 
 	Add Fish Form
