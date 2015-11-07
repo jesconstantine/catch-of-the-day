50a60156d1fb0cd42a1d15da4f5a0415f4711ca8
Jes Constantine <jesconstantine@gmail.com>
2015-11-06 19:36:08 -0500

    display order state with jsx

--- scripts/main.js
+++ scripts/main.js
@@ -53,13 +53,13 @@ var App = React.createClass ({
 					<Header tagline="Fresh Seafood Market" />
 					<ul className="list-of-fishes">
 						{
-							//Object.keys returns array of all keys in object
+							// Object.keys returns array of all keys in object
 							// run renderFish once for every fish that we have in state
 							Object.keys(this.state.fishes).map(this.renderFish)
 						}
 					</ul>
 				</div>
-				<Order />
+				<Order fishes={this.state.fishes} order={this.state.order} />
 				<Inventory addFish={this.addFish} loadSamples={this.loadSamples} />
 			</div>
 		)
@@ -167,9 +167,48 @@ var Header = React.createClass({
 */
 
 var Order = React.createClass({
-	render : function() {
+	renderOrder : function(key) {
+		var fish = this.props.fishes[key];
+		var count = this.props.order[key];
+
+		if(!fish) {
+			return <li key={key}>Sorry, fish is no longer available</li>
+		}
+
 		return (
-			<p>Order</p>
+			<li>
+				<span>{count}</span>lbs
+				{fish.name}
+				<span className="price">{helpers.formatPrice(count * fish.price)}</span>
+			</li>
+		)
+	},
+	render : function() {
+		var orderIds = Object.keys(this.props.order);
+		
+		// iterate over all orders and add new amount spent to previous total
+		var total = orderIds.reduce((prevTotal, key)=> {
+			var fish = this.props.fishes[key];
+			var count = this.props.order[key];
+			var isAvailable = fish && fish.status === 'available';
+
+			if(fish && isAvailable) {
+				return prevTotal + (count * parseInt(fish.price) || 0)
+			}
+
+			return prevTotal;
+		}, 0);
+		return (
+			<div className="order-wrap">
+				<h2 className="order-title">Your Order</h2>
+				<ul className="order">
+					{orderIds.map(this.renderOrder)}
+					<li className="total">
+						<strong>Total:</strong>
+						{helpers.formatPrice(total)}
+					</li>
+				</ul>
+			</div>
 		)
 	}
 });
