c51089d72084ee4f286ef5c64367ddf66e148cba
Jes Constantine <jesconstantine@gmail.com>
2015-11-07 08:05:29 -0500

    add bidirectional data flow to inventory with linkState mixin

--- scripts/main.js
+++ scripts/main.js
@@ -15,11 +15,14 @@ var helpers = require('./helpers');
 var Rebase = require('re-base');
 var base = Rebase.createClass('https://catch-of-the-day-jc.firebaseio.com/');
 
+var Catalyst = require('react-catalyst');
+
 /*
 	App
 */
 
 var App = React.createClass ({	
+	mixins : [Catalyst.LinkedStateMixin],
 	// run getInitialState before creates component and populate with whatever is returned
 	getInitialState : function() {
 		return {
@@ -84,7 +87,7 @@ var App = React.createClass ({
 					</ul>
 				</div>
 				<Order fishes={this.state.fishes} order={this.state.order} />
-				<Inventory addFish={this.addFish} loadSamples={this.loadSamples} />
+				<Inventory addFish={this.addFish} loadSamples={this.loadSamples} fishes={this.state.fishes} linkState={this.linkState} />
 			</div>
 		)
 	}
@@ -244,10 +247,27 @@ var Order = React.createClass({
 
 // {...this.props} spread, pass down all props
 var Inventory = React.createClass({
+	renderInventory : function (key) {
+		var linkState = this.props.linkState;
+		return (
+			<div className="fish-edit" key={key}>
+				<input type="text" valueLink={linkState('fishes.' + key + '.name')} />
+				<input type="text" valueLink={linkState('fishes.' + key + '.price')} />
+				<select valueLink={linkState('fishes.'+ key + '.status')}>
+					<option value="available">Fresh!</option>
+					<option value="unavailable">Sold Out!</option>
+				</select>
+				<textarea valueLink={linkState('fishes.' + key + '.desc')}></textarea>
+				<input type="text" valueLink={linkState('fishes.' + key + '.image')} />
+				<button>Remove Fish</button>
+			</div>
+		)
+	},
 	render : function() {
 		return (
 			<div>
 				<h2>Inventory</h2>
+				{Object.keys(this.props.fishes).map(this.renderInventory)}
 
 				<AddFishForm {...this.props} />
 				<button onClick={this.props.loadSamples}>Load Sample Fishes</button>
